a73bbeccbc659cbf36970141696e41998b029d3d
Hans Landgrebe <hlxbl20@gmail.com>
2023-07-25 14:43:30 -0500

    Created Title Screen
    Title Screen Created, assets added, data persistence.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //ENCAPSULATION
+    public static GameManager Instance { get; private set; }
+    public int chosenCar { get; private set; }
+    private void Awake()
+    {
+        Singleton();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Singleton()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartGame(int carChoice)
+    {
+        AssignCar(carChoice);
+        SceneManager.LoadScene(1);
+    }
+
+    private void AssignCar(int carChoice)
+    {
+        chosenCar = carChoice;
+    }
+}
